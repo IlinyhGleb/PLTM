768ee2cdebf40c8714db2d16f46ee1d2608408d9
Gleb Ilinyh <gleb@ilinyh.ru>
2023-09-14 22:33:05 +0500

    add lab2

--- labs/lab1.h
+++ labs/lab1.h
@@ -2,3 +2,4 @@
 #include <stdlib.h>
 
 int lab1(int a, int b);
+int lab2();
---
+++ labs/lab2.c
@@ -0,0 +1,27 @@
+#include "lab1.h"
+#include <stdio.h>
+
+int lab2()
+{
+    int a;
+    int b;
+    int c;
+    int x;
+
+    a = 1;
+    b = 2;
+    c = 3;
+
+    x = (c-b)/a;
+
+    printf("x=", $x)
+
+	return 0;
+}
+
+int main()
+{
+    lab2()
+
+    return 0;
+}

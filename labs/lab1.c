95b7e13abc19e252f15833f6e06858211cd7813c
Gleb Ilinyh <gleb@ilinyh.ru>
2023-09-14 22:51:39 +0500

    move lab2 into lab1.c

--- labs/lab1.c
+++ labs/lab1.c
@@ -6,9 +6,29 @@ int lab1(int a, int b)
 	return a + b;
 }
 
+int lab2()
+{
+    int a;
+    int b;
+    int c;
+    int x;
+
+    a = 1;
+    b = 2;
+    c = 3;
+
+    x = (c-b)/a;
+
+    printf("x=%d\n", x);
+
+	return 0;
+}
+
+
 int main()
 {
     printf("Hello World");
+    lab2();
 
     return 0;
 }

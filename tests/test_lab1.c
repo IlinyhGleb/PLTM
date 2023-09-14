be12290b812288a00f3056215ef8cf15747a56ff
IlinyhGleb <97687852+IlinyhGleb@users.noreply.github.com>
2023-09-14 20:47:34 +0500

    Create test_lab1.c

---
+++ tests/test_lab1.c
@@ -0,0 +1,23 @@
+
+//#include "../../../algorithms/sorting/sorting.h"
+//#include "main.h"
+
+/**
+ * test_bubble_sort - Test bubble sort
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int test_lab1(void)
+{
+	int a;
+	int b;
+  int c;
+
+  a = 4;
+  b = 3;
+  c = lab1(a, b)
+	if (c == 7)
+    return (0);
+  
+	return (-1);
+}

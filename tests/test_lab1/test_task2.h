6bc573e7474df9c4539288f5260df104f8b1ffdd
Gleb Ilinyh <gleb@ilinyh.ru>
2023-09-15 00:59:51 +0500

    update structure

--- tests/test_lab1/test_task2.h
+++ tests/test_lab1/test_task2.h
@@ -1,11 +1,9 @@
 #ifndef TEST_LAB1_H
 #define TEST_LAB1_H
 
-#include "../../labs/lab1/lab1.h"
+#include "../../labs/lab1/task2.h"
 
 /* Test functions */
-int test_task1(void);
 int test_task2(void);
-
 
 #endif /* TEST_LAB1_H */
